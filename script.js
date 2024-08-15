4bd7d75613964e77775dbab0815eac542a4b63d1
Tristan W. <tris_went@proton.me>
2024-08-15 03:11:52 -0400

    fixing le buggy bug

--- script.js
+++ script.js
@@ -9,13 +9,15 @@ let currentYear = new Date().getFullYear();
 currentYearElem.textContent = currentYear;
 
 // get all a links
-const as = document.querySelectorAll('a[href^=#]');
+const as = document.querySelectorAll('a');
 
 // add click event listener to each a link
 
 as.forEach(a => {
   a.addEventListener('click', function (e) {
     e.preventDefault(); // prevent the default link behavior
+
+    if (!a.getAttribute('href').startsWith('#')) return; // skip if the link does not lead to a section
 
     // get target element
     const targetId = a.getAttribute('href').slice(1);
